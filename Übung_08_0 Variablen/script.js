b0f46c3da9cb7c0372687a92e2291c26db90664e
Aaronfk07 <ghost.aaron@gmx.at>
2023-12-11 11:45:51 +0100

    Variablen_Übung_finished

---
+++ Übung_08_0 Variablen/script.js
@@ -0,0 +1,18 @@
+ let Banana = "Banana";
+ let Apple = "Apple";
+ 
+
+ let bananaPriceperKilo = 2.14;
+ let applePriceperKIlo = 3.43;
+
+ let bananaweight = 0.34;
+ let appleweight = 0.22;
+
+ console.log("Preis von 8 Äpfeln ist " + ( appleweight*applePriceperKIlo)*8 + "€");
+ console.log("Preis von 17 Bananen ist " + (bananaweight*bananaPriceperKilo)* 17 + "€");
+
+ console.log("Preis von 1Tonne Äpfel" + (1000*applePriceperKIlo) + " € ");
+
+ console.log("Preis von 1Tonne Bananen" + (1000*bananaPriceperKilo) + " € ");
+
+ 
