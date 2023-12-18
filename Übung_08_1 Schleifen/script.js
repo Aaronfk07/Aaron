bcc3be6c7fe4cb794d11f9f15cee1a21329c6794
Aaronfk07 <ghost.aaron@gmx.at>
2023-12-18 11:21:19 +0100

    Übung_08_1 Schleifen

---
+++ Übung_08_1 Schleifen/script.js
@@ -0,0 +1,38 @@
+console.log("test");
+
+// Schleife
+
+for(let count = 0; count<10;count ++){
+    console.log("Adrian der Arme ")
+}
+console.log("Fertig");
+
+for(let count = 10; count>=0; count --){
+    console.log( count + "Adrian der Arme")
+
+}
+console.log("fertig");
+
+for(let count = 7; count<45;count ++){
+    console.log( count + "Adrian der Arme ")
+}
+console.log("Fertig");
+
+for(let count = 9; count<99;count ++){
+    console.log( count + "Adrian der Arme ")
+}
+console.log("Fertig");
+for(let count = 0; count <10; count ++){
+    console.log(count + "Zahl")
+
+}
+for(let count = 0; count<=8; count ++){
+    console.log(count + "Aaron")
+
+}
+
+for(let count = 10; count>=0; count --){
+    console.log( count + "Zahl")
+}
+
+console.log("fertig");
