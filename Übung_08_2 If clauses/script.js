30f7e3f8483b81e40a1f3f8096414387f1def570
Aaronfk07 <ghost.aaron@gmx.at>
2023-12-18 11:52:12 +0100

    Übung_08_03 If-Clauses

---
+++ Übung_08_2 If clauses/script.js
@@ -0,0 +1,18 @@
+let number = 187;
+
+if (number<20){
+    console.log("Die Zahl ist Größer als 187");
+}   else {
+    console.log("else" + number)
+
+}
+
+let nummer = 20;
+
+if(nummer<20){
+    console.log("Mini")
+} else if(nummer >25 && nummer < 50) {
+    console.log("medium")
+}  else if (nummer>50){
+    console.log("large")
+}
