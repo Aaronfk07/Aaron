587306ea9291f6a1b0b5ebee75f58673436c6905
Aaronfk07 <ghost.aaron@gmx.at>
2024-01-22 11:21:06 +0100

    TEST_02

---
+++ TEST_02/script.js
@@ -0,0 +1,19 @@
+let randomNumber = Math.random() * 35 + 16;
+// bis wohin die random number geht wenn wir von 35 bis zwanzig wollen eif + 16
+let random = Math.floor(randomNumber);
+
+console.log(randomNumber);
+
+if(randomNumber = 35){
+    console.log("Ohh");
+}
+else if(randomNumber > 38 && randomNumber < 36 ){
+    console.log(randomNumber)
+} 
+else if(randomNumber = 50 && randomNumber = 51);
+{
+    console.log("Jupidu")
+} 
+else if(randomNumber = 49){
+    console.log("Die Zahl 49 ist" + random)
+}
