5c446cb2937f633f5b7770f13ea7e28036407d59
Aaronfk07 <ghost.aaron@gmx.at>
2024-01-22 11:20:53 +0100

    TEST_02

---
+++ Random_number/script.js
@@ -0,0 +1,5 @@
+let randomNumber = Math.random() * 40;
+// bis wohin die random number geht wenn wir von 10 bis zwanzig wollen eif + 10
+let random = Math.floor(randomNumber);
+
+console.log(random);
