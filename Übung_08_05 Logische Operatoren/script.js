10af33a091e7eb1aeabe0a0a1e8a2cc206df5be5
Aaronfk07 <ghost.aaron@gmx.at>
2024-03-04 11:28:27 +0100

    Logische Operatoren

---
+++ Übung_08_05 Logische Operatoren/script.js
@@ -0,0 +1,14 @@
+
+let randomNumber = Math.random() + 100;
+let randomNumber2 = Math.random() + 100;
+
+let random = Math.floor(randomNumber);
+let random2 = Math.floor(randomNumber2);
+
+if(random > random2 ) && (random > 50); {
+    console.log("Zahl 1 ist größer als Zahl 2" + "Mini");
+} if else(random > 30) || (random2 > 30); {
+    console.log("Einer der beiden Zahlen ist 30");
+} if else(random < 50) && (random != 50); {
+    console.log("Erste Zahl klein" + "Zweite Zahl kein 50ziger");
+}
