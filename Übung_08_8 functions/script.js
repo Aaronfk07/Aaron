f9481b13bafcaee588ab65b6a3b84e810038a805
Aaronfk07 <ghost.aaron@gmx.at>
2024-03-11 11:52:23 +0100

    functions

---
+++ Übung_08_8 functions/script.js
@@ -0,0 +1,8 @@
+function printMenu(name, age, place,) {
+    console.log("Habidere");
+    console.log("Willkommen" + name + age + place);
+    console.log("Habidere");
+}
+
+printMenu("Peter", 58,"Bangs");
+
